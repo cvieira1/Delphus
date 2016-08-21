6cfd8ade1d52e12ae6525bc8e15c3808694f94e2
cvieira1 <anjoscaio@yahoo.com>
2016-08-21 15:11:16 -0300

    new bkgrounds/modified scripts/new sentences/new chars

---
+++ projetodelphus/scripts/missionfunctions.php
@@ -0,0 +1,36 @@
+<?php
+     include("functions.php");
+    ?>
+<script>
+        function answer1(){
+          hide(a,b,'sentence1');
+          show(a,b,'sentence2');
+          hide(a,b,'sentence3');
+          hide(a,b,'sentence4');
+          hide(a,b,'seta1');
+         }
+        function answer2(){
+          hide(a,b,'sentence1');
+          hide(a,b,'sentence2');
+          show(a,b,'sentence3');
+          hide(a,b,'sentence4');
+          hide(a,b,'seta1');         
+         }
+        function answer3(){
+          hide(a,b,'sentence1');
+          hide(a,b,'sentence2');
+          hide(a,b,'sentence3');
+          show(a,b,'sentence4');
+          show(a,b,'seta1');
+         }
+        function normal(){
+          hide(a,b,'sentence1');
+          hide(a,b,'sentence2');
+          hide(a,b,'sentence3');
+          hide(a,b,'sentence4');
+          hide(a,b,'seta1');
+          hide(a,b,'villager1');
+          hide(a,b,'buttons1');
+          show(a,b,'slum');
+         }
+</script>
--- projetodelphus/scripts/village_pub.php
+++ projetodelphus/scripts/village_pub.php
@@ -1,6 +1,14 @@
 <html>
       <head>
+            <style>
+                   img.bg{                    
+                   position:absolute;
+                   z-index:-100;
+                    top:0;left:0;bottom:0;right:0;
+                    }
+            </style>
       </head>
       <body>
+            <img src="../bkground/pub1.png" class="bg">
       </body>
 </html>
